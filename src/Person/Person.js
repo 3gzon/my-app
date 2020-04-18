5887468ed0490b88b615c4138ce795fbca0a5042
Egzon <egzon@starlabspro.com>
2020-04-18 19:11:15 +0200

    feat: using media queries from radium library

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Radium from 'radium'
-
+import './Person.css'
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
     return (
         <div className="person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
