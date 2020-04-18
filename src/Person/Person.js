bb6026ebe30f41c3807e28333be9ac73c389663c
Egzon <egzon@starlabspro.com>
2020-04-18 14:40:24 +0200

    feat: use state and change state function

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Person = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}>I am a {props.name} and I am {props.age} old</p>
+            <p>{props.children}</p>
+        </div>
+    )
+}
+
+export default Person
